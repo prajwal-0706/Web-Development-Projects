a42d341da1583d9e0fbfeb678ee4410069c610b4
prajwal-0706 <117188729+prajwal-0706@users.noreply.github.com>
2022-12-03 18:38:23 +0530

    Update index.js

--- JavaScript Projects/Drum Kit/index.js
+++ JavaScript Projects/Drum Kit/index.js
@@ -20,32 +20,32 @@ document.addEventListener('keypress',(e) => {
 function playmusic(abc){
     switch (abc){
         case 'w':
-            var audio = new Audio('/sounds/tom-1.mp3');
+            var audio = new Audio('./sounds/tom-1.mp3');
             audio.play();
             break;
 
         case 'a':
-            var audio = new Audio('/sounds/tom-2.mp3');
+            var audio = new Audio('./sounds/tom-2.mp3');
             audio.play();
             break;
         case 's':
-            var audio = new Audio('/sounds/tom-3.mp3');
+            var audio = new Audio('./sounds/tom-3.mp3');
             audio.play();
             break;
         case 'd':
-            var audio = new Audio('/sounds/tom-4.mp3');
+            var audio = new Audio('./sounds/tom-4.mp3');
             audio.play();
             break;
         case 'j':
-            var audio = new Audio('/sounds/kick-bass.mp3');
+            var audio = new Audio('./sounds/kick-bass.mp3');
             audio.play();
             break;
         case 'k':
-            var audio = new Audio('/sounds/crash.mp3');
+            var audio = new Audio('./sounds/crash.mp3');
             audio.play();
             break;
         case 'l':
-            var audio = new Audio('/sounds/snare.mp3');
+            var audio = new Audio('./sounds/snare.mp3');
             audio.play();
             break;
     }
