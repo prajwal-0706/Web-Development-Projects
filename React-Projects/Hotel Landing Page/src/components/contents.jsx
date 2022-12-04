6860899b3be62ab754f80b3fc13a01e4a2e24698
prajwal-0706 <117188729+prajwal-0706@users.noreply.github.com>
2022-12-04 22:57:10 +0530

    Add files via upload

---
+++ React-Projects/Hotel Landing Page/src/components/contents.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+let heading = [
+  "OUR HOTELS","DINING","WEDDING","GIFTCADS","MANAGE BOOKINGS","SIGN IN"
+]
+
+function Content(){
+  return(
+    <a> 
+      {headings}
+    </a>
+  )
+}
+
+export deafult Content
