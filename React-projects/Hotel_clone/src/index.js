6386cef0a55d8a9fc03fedbac7138e33a0cf0612
prajwal-0706 <117188729+prajwal-0706@users.noreply.github.com>
2022-12-14 17:12:21 +0000

    update

--- React-projects/Hotel_clone/src/index.js
+++ React-projects/Hotel_clone/src/index.js
@@ -1,48 +1,8 @@
 
-let sliderImages = document.querySelectorAll(".slide"),
-  arrowLeft = document.querySelector("#arrow-left"),
-  arrowRight = document.querySelector("#arrow-right"),
-  current = 0;
+// import react from 'react';
 
-function reset() {
-  for (let i = 0; i < sliderImages.length; i++) {
-    sliderImages[i].style.display = "none";
-  }
-}
-
-function startSlide() {
-  reset();
-  sliderImages[0].style.display = "block";
-}
-
-function slideLeft() {
-  reset();
-  sliderImages[current - 1].style.display = "block";
-  current--;
-}
-
-function slideRight() {
-  reset();
-  sliderImages[current + 1].style.display = "block";
-  current++;
-}
-
-arrowLeft.addEventListener("click", function () {
-  if (current === 0) {
-    current = sliderImages.length;
-  }
-  slideLeft();
-});
-
-arrowRight.addEventListener("click", function () {
-  if (current === sliderImages.length - 1) {
-    current = -1;
-  }
-  slideRight();
-});
-
-startSlide();
+// import  ReactDOM  from 'react-dom';
 
 
 
-
+// ReactDOM.render( <h1> Hello </h1>,document.getElementById('root') )
