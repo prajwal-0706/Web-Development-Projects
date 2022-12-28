1c7b045e306625e98009b68a3cbac385a4556bc3
prajwal-0706 <117188729+prajwal-0706@users.noreply.github.com>
2022-12-28 22:26:57 +0000

    update

--- JavaScript Projects/Tic Tac Toe Game/index.js
+++ JavaScript Projects/Tic Tac Toe Game/index.js
@@ -38,80 +38,84 @@ cells.forEach(element => {
                 flag = true;
             }
             // first Row Win
-            if((b1.textContent === 'X'&& b2.textContent === "X") && (b2.textContent === "X" && b3.textContent === "X")){
+            if((b1.textContent === 'X'&& b2.textContent === 'X') && (b2.textContent === 'X' && b3.textContent === 'X')){
                 player_1_won();
                 setTimeout(clear,3000);            
             }
-            if((b1.textContent === 'O'&& b2.textContent === "O") && (b2.textContent === "O" && b3.textContent === "O")){
+            if((b1.textContent === 'O'&& b2.textContent === 'O') && (b2.textContent === 'O' && b3.textContent === 'O')){
                 player_2_won();
                 setTimeout(clear,3000);
             }
             // Second row win
-            if((b4.textContent === 'X'&& b5.textContent === "X") && (b5.textContent === "X" && b6.textContent === "X")){
+            if((b4.textContent === 'X'&& b5.textContent === 'X') && (b5.textContent === 'X' && b6.textContent === 'X')){
                 player_1_won();
                 setTimeout(clear,3000);
             }
-            if((b4.textContent === 'O'&& b5.textContent === "O") && (b5.textContent === "O" && b6.textContent === "O")){
+            if((b4.textContent === 'O'&& b5.textContent === 'O') && (b5.textContent === 'O' && b6.textContent === 'O')){
                 player_2_won();
                 setTimeout(clear,3000);
             }
 
             //Third Row Win
-            if((b7.textContent === 'X'&& b8.textContent === "X") && (b8.textContent === "X" && b9.textContent === "X")){
+            if((b7.textContent === 'X'&& b8.textContent === 'X') && (b8.textContent ==='X' && b9.textContent === 'X')){
                 player_1_won();
                 setTimeout(clear,3000);
             }
-            if((b7.textContent === 'O'&& b8.textContent === "O") && (b8.textContent === "O" && b9.textContent === "O")){
+            if((b7.textContent === 'O'&& b8.textContent === 'O') && (b8.textContent === 'O' && b9.textContent === 'O')){
                 player_2_won();
                 setTimeout(clear,3000);
             }
 
             //first col win
-            if((b1.textContent === 'X'&& b4.textContent === "X") && (b4.textContent === "X" && b7.textContent === "X")){
+            if((b1.textContent === 'X'&& b4.textContent === 'X') && (b4.textContent ==='X' && b7.textContent === 'X')){
                 player_1_won();
                 setTimeout(clear,3000);
             }
-            if((b1.textContent === 'O'&& b4.textContent === "O") && (b4.textContent === "O" && b7.textContent === "O")){
+            if((b1.textContent === 'O'&& b4.textContent === 'O') && (b4.textContent === 'O' && b7.textContent === 'O')){
                 player_2_won();
                 setTimeout(clear,3000);
             }
             // Second col win
-            if((b2.textContent === 'X'&& b5.textContent === "X") && (b5.textContent === "X" && b8.textContent === "X")){
+            if((b2.textContent === 'X'&& b5.textContent === 'X') && (b5.textContent === 'X' && b8.textContent === 'X')){
                 player_1_won();
                 setTimeout(clear,3000);
             }
-            if((b2.textContent === 'O'&& b5.textContent === "O") && (b5.textContent === "O" && b8.textContent === "O")){
+            if((b2.textContent === 'O'&& b5.textContent === 'O') && (b5.textContent === 'O' && b8.textContent === 'O')){
                 player_2_won();
                 setTimeout(clear,3000);
             }
             //third col win
-            if((b3.textContent === 'X'&& b6.textContent === "X") && (b6.textContent === "X" && b9.textContent === "X")){
+            if((b3.textContent === 'X'&& b6.textContent === 'X') && (b6.textContent === 'X' && b9.textContent === 'X')){
                 player_1_won();
                 setTimeout(clear,3000);
             }
-            if((b3.textContent === 'O'&& b6.textContent === "O") && (b6.textContent === "O" && b9.textContent === "O")){
+            if((b3.textContent === 'O'&& b6.textContent === 'O') && (b6.textContent === 'O' && b9.textContent === 'O')){
                 player_2_won();
                 setTimeout(clear,3000);
             }
 
             // First Diagonal win
-            if((b1.textContent === 'X'&& b5.textContent === "X") && (b5.textContent === "X" && b9.textContent === "X")){
+            if((b1.textContent === 'X'&& b5.textContent === 'X') && (b5.textContent === 'X' && b9.textContent === 'X')){
                 player_1_won();
                 clear();
             }
-            if((b1.textContent === 'O'&& b5.textContent === "O") && (b5.textContent === "O" && b9.textContent === "O")){
+            if((b1.textContent === 'O'&& b5.textContent === 'O') && (b5.textContent === 'O' && b9.textContent === 'O')){
                 player_2_won();
-                clear();
+                 setTimeout(clear,3000);
             }
 
             // second diagonal win
-            if((b3.textContent === 'X'&& b5.textContent === "X") && (b5.textContent === "X" && b7.textContent === "X")){
+            if((b3.textContent === 'X'&& b5.textContent === 'X') && (b5.textContent ==='X' && b7.textContent === 'X')){
                 player_1_won();
-                clear();
+                 setTimeout(clear,3000);
             }
-            if((b3.textContent === 'O'&& b5.textContent === "O") && (b5.textContent === "O" && b7.textContent === "O")){
+            if((b3.textContent === 'O'&& b5.textContent === 'O') && (b5.textContent === 'O' && b7.textContent === 'O')){
                 player_2_won();
-                clear();
+                 setTimeout(clear,3000);
+            }
+            if((b1.innerHTML === 'X'|| b1.innerHTML === 'O') && (b2.innerHTML === 'X'|| b2.innerHTML === 'O') && (b3.innerHTML === 'X'|| b3.innerHTML ==='O') && (b4.innerHTML === 'X'|| b4.innerHTML === 'O') && (b5.innerHTML === 'X'|| b5.innerHTML === 'O') && (b6.innerHTML === "X"|| b6.innerHTML === 'O') && (b7.innerHTML === 'X'|| b7.innerHTML === 'O') && (b8.innerHTML === 'X'|| b8.innerHTML === 'O') && (b9.innerHTML === 'X'|| b9.innerHTML === 'O') ){
+                tie_or_wot();
+                setTimeout(clear,3000);
             }
         }
 
@@ -139,4 +143,11 @@ function player_2_won(){
     right_image.style.opacity = '1' ;
     player_display.style.transition = "0.8s";
     player_display.innerHTML = "<h3>Player 2 Won ✨</h3>"
+}
+
+function tie_or_wot(){
+    player_display.innerHTML = "<h3>Well Played Both...Its a Tie</h3>";
+    left_image.style.opacity = '1' ;
+    right_image.style.opacity = '1' ;
+    player_display.style.transition = "0.8s";
 }
