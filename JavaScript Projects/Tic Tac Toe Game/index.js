2a8bf085142ce797b5bb2b7dda003ca5f8d207bc
prajwal-0706 <117188729+prajwal-0706@users.noreply.github.com>
2022-12-28 17:24:51 +0000

    update

--- JavaScript Projects/Tic Tac Toe Game/index.js
+++ JavaScript Projects/Tic Tac Toe Game/index.js
@@ -2,65 +2,20 @@ const cells = document.querySelectorAll('.game-box');
 const Player1 = document.querySelector('.player1');
 const Player2 = document.querySelector('.player2');
 
-let player1S = [];
-let player2S = [];
-
-[3,4,5];
-const winningComb = [
-    [2,4,6],
-    [0,1,2],
-    [0,3,6],
-    [6,7,8],
-    [2,5,8],
-    [1,4,7],
-    [0,4,8]
-    
-]
-
-
-function handleClick(){
-    cells[i].textContent = " X ";
-    alert("Hello");
-}
-
 let flag = true;
 
-for(let i = 0 ; i<cells.length ; i++){
- cells[i].addEventListener('click',() => {
-    if(flag){
-        player1(i);
-        
-    }
-    else{
-        player2(i);
-    }
-    checkWin();
- });
-}
 
-function player1 (i){
-    cells[i].textContent = "X";
-    flag = false;
-    player1S.push(i);
-}
-function player2 (i){
-    cells[i].textContent = "O";
-    flag = true;
-    player2S.push(i);  
-}
+cells.forEach(element => {
+    element.addEventListener('click',() =>{
 
-
-function checkWin(){
-    winningComb.find((item) => {
-        if(item.filter((i) => player1S.includes(i).length === 3)){
-            alert("Player 1 Won");
-            return item;
+        if(flag){
+            element.innerHTML = "X";
+            flag = false;
         }
-        else if(item.filter((i) => player2S.includes(i).length === 3)){
-            alert("Player 2 Won");
-            
+        else{
+            element.innerHTML = "O";
+            flag = true;
         }
-        return
-    })
-}
+    });
+});
 
