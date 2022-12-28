5ea4aea09862fd15f75476dc42e434bd35f02f5e
prajwal-0706 <117188729+prajwal-0706@users.noreply.github.com>
2022-12-28 19:17:48 +0000

    update

--- JavaScript Projects/Tic Tac Toe Game/index.js
+++ JavaScript Projects/Tic Tac Toe Game/index.js
@@ -7,15 +7,17 @@ let flag = true;
 
 cells.forEach(element => {
     element.addEventListener('click',() =>{
+        if(element.innerHTML !== 'X' && element.innerHTML !== 'O' ){
+            if(flag){
+                element.innerHTML = "X";
+                flag = false;
+            }
+            else{
+                element.innerHTML = "O";
+                flag = true;
+            }
+        }
 
-        if(flag){
-            element.innerHTML = "X";
-            flag = false;
-        }
-        else{
-            element.innerHTML = "O";
-            flag = true;
-        }
     });
 });
 
