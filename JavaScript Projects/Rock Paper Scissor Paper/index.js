d1936da7cad661eec5ebb55f12669e2e7c0aeff5
prajwal-0706 <117188729+prajwal-0706@users.noreply.github.com>
2023-01-07 12:27:26 +0000

    Update

--- JavaScript Projects/Rock Paper Scissor Paper/index.js
+++ JavaScript Projects/Rock Paper Scissor Paper/index.js
@@ -2,15 +2,78 @@ const image = document.querySelectorAll('#image');
 const img_1 = document.querySelector('.image-1');
 const img_2 = document.querySelector('.image-2');
 const img_3 = document.querySelector('.image-3');
+const button = document.querySelector('h1');
 
-const Arr = ['Rock', 'Paper', 'Scissor'];
+button.addEventListener('click', Dance);
 
-let no = Math.floor(Math.random() * Arr.length );
 
-console.log(no);
 
-image.forEach((Element) => {
-    Element.addEventListener('click',() => {
-        alert(Element.ariaValueMax);
-    }); 
-});
+function Img1(){
+    img_1.style.opacity = '1';
+    img_2.style.opacity = '0';
+    img_3.style.opacity = '0';
+}
+
+function Img2(){
+    img_1.style.opacity = '0';
+    img_2.style.opacity = '1';
+    img_3.style.opacity = '0';
+}
+
+function Img3(){
+    img_1.style.opacity = '0';
+    img_2.style.opacity = '0';
+    img_3.style.opacity = '1';
+}
+
+function Dance(){
+    const Arr = ['Rock', 'Paper', 'Scissor'];
+    var no = Arr[Math.floor(Math.random() * Arr.length)];
+    
+
+    if(no === 'Rock'){
+        for(let i = 0 ; i < 10 ; i++){
+            Img2();
+            setTimeout(Img3,250);
+            setTimeout(Img1, 500);
+            setTimeout(Img2,750);
+            setTimeout(Img1, 1000);
+            setTimeout(Img3,1250);
+            setTimeout(Img1, 1500);
+            setTimeout(Img2,1750);
+            setTimeout(Img3, 2000);
+            setTimeout(Img2,2250);
+            setTimeout(Img1, 2500);
+        }
+    }
+    else if(no === 'Paper'){
+        for(let i = 0 ; i < 10 ; i++){
+            Img2();
+            setTimeout(Img3,250);
+            setTimeout(Img1, 500);
+            setTimeout(Img2,750);
+            setTimeout(Img1, 1000);
+            setTimeout(Img3,1250);
+            setTimeout(Img1, 1500);
+            setTimeout(Img2,1750);
+            setTimeout(Img3, 2000);
+            setTimeout(Img2,2250);
+            setTimeout(Img1, 2500);
+        }
+    }
+    else{
+        for(let i = 0 ; i < 10 ; i++){
+            Img2();
+            setTimeout(Img3,250);
+            setTimeout(Img1, 500);
+            setTimeout(Img2,750);
+            setTimeout(Img1, 1000);
+            setTimeout(Img3,1250);
+            setTimeout(Img1, 1500);
+            setTimeout(Img2,1750);
+            setTimeout(Img3, 2000);
+            setTimeout(Img2,2250);
+            setTimeout(Img1, 2500);
+        }
+    }
+}
