7bc3a3fae9383d3dbffc00bc0bfd19677d010ea0
prajwal-0706 <117188729+prajwal-0706@users.noreply.github.com>
2023-01-07 10:43:34 +0000

    Update

--- JavaScript Projects/Rock Paper Scissor Paper/index.js
+++ JavaScript Projects/Rock Paper Scissor Paper/index.js
@@ -1,7 +1,16 @@
+const image = document.querySelectorAll('#image');
 const img_1 = document.querySelector('.image-1');
 const img_2 = document.querySelector('.image-2');
 const img_3 = document.querySelector('.image-3');
 
-console.log(img_1.ariaValueMax)
-console.log(img_2.ariaValueMax)
-console.log(img_3.ariaValueMax)
+const Arr = ['Rock', 'Paper', 'Scissor'];
+
+let no = Math.floor(Math.random() * Arr.length );
+
+console.log(no);
+
+image.forEach((Element) => {
+    Element.addEventListener('click',() => {
+        alert(Element.ariaValueMax);
+    }); 
+});
