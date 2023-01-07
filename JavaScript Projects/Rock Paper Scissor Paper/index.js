441041fa089b2bb887bcde52f217251345db8830
prajwal-0706 <117188729+prajwal-0706@users.noreply.github.com>
2023-01-07 09:15:27 +0000

    Update

---
+++ JavaScript Projects/Rock Paper Scissor Paper/index.js
@@ -0,0 +1,7 @@
+const img_1 = document.querySelector('.image-1');
+const img_2 = document.querySelector('.image-2');
+const img_3 = document.querySelector('.image-3');
+
+console.log(img_1.ariaValueMax)
+console.log(img_2.ariaValueMax)
+console.log(img_3.ariaValueMax)
