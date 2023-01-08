45ad5aed83fdf3477b70ccf0c562db1f957a3432
prajwal-0706 <117188729+prajwal-0706@users.noreply.github.com>
2023-01-08 05:37:33 +0000

    Update

--- JavaScript Projects/Rock Paper Scissor Paper/index.js
+++ JavaScript Projects/Rock Paper Scissor Paper/index.js
@@ -2,44 +2,37 @@ const image = document.querySelectorAll('#image');
 const img_1 = document.querySelector('.image-1');
 const img_2 = document.querySelector('.image-2');
 const img_3 = document.querySelector('.image-3');
-
+const button_1 = document.querySelectorAll('button'); 
 const Img_1 = document.querySelector('.imag-1');
 const Img_2 = document.querySelector('.imag-2');
 const Img_3 = document.querySelector('.imag-3');
-
 const button = document.querySelector('.PlayerScore');
 const but = document.querySelector('.ComputerScore');
-
-but.addEventListener('click', dance);
-
-button.addEventListener('click', Dance);
-
+let choice = '';
+const Arr = ['Rock', 'Paper', 'Scissor'];
+var no = Arr[Math.floor(Math.random() * Arr.length)];
 
 
-function Img1(){
+function Img1(){ // Paper
     img_1.style.opacity = '1';
     img_2.style.opacity = '0';
     img_3.style.opacity = '0';
 }
 
-function Img2(){
+function Img2(){ // Scissor
     img_1.style.opacity = '0';
     img_2.style.opacity = '1';
     img_3.style.opacity = '0';
 }
 
-function Img3(){
+function Img3(){ //Rock
     img_1.style.opacity = '0';
     img_2.style.opacity = '0';
     img_3.style.opacity = '1';
 }
 
 function Dance(){
-    const Arr = ['Rock', 'Paper', 'Scissor'];
-    var no = Arr[Math.floor(Math.random() * Arr.length)];
-    
-
-    if(no === 'Rock'){
+    if(choice === 'Rock'){
         for(let i = 0 ; i < 10 ; i++){
             Img2();
             setTimeout(Img3,250);
@@ -49,12 +42,12 @@ function Dance(){
             setTimeout(Img3,1250);
             setTimeout(Img1, 1500);
             setTimeout(Img2,1750);
-            setTimeout(Img3, 2000);
+            setTimeout(Img1, 2000);
             setTimeout(Img2,2250);
-            setTimeout(Img1, 2500);
+            setTimeout(Img3, 2500);
         }
     }
-    else if(no === 'Paper'){
+    else if(choice === 'Paper'){
         for(let i = 0 ; i < 10 ; i++){
             Img2();
             setTimeout(Img3,250);
@@ -77,27 +70,28 @@ function Dance(){
             setTimeout(Img2,750);
             setTimeout(Img1, 1000);
             setTimeout(Img3,1250);
-            setTimeout(Img1, 1500);
-            setTimeout(Img2,1750);
-            setTimeout(Img3, 2000);
-            setTimeout(Img2,2250);
-            setTimeout(Img1, 2500);
+            setTimeout(Img2, 1500);
+            setTimeout(Img1,1750);
+            setTimeout(Img2, 2000);
+            setTimeout(Img3,2250);
+            setTimeout(Img2, 2500);
         }
     }
 }
-function img1(){
+
+function img1(){ // Paper
     Img_1.style.opacity = '1';
     Img_2.style.opacity = '0';
     Img_3.style.opacity = '0';
 }
 
-function img2(){
+function img2(){ // Scissors
     Img_1.style.opacity = '0';
     Img_2.style.opacity = '1';
     Img_3.style.opacity = '0';
 }
 
-function img3(){
+function img3(){ //rock
     Img_1.style.opacity = '0';
     Img_2.style.opacity = '0';
     Img_3.style.opacity = '1';
@@ -105,10 +99,6 @@ function img3(){
 
 
 function dance(){
-    const Arr = ['Rock', 'Paper', 'Scissor'];
-    var no = Arr[Math.floor(Math.random() * Arr.length)];
-    
-
     if(no === 'Rock'){
         for(let i = 0 ; i < 10 ; i++){
             img2();
@@ -119,9 +109,9 @@ function dance(){
             setTimeout(img3,1250);
             setTimeout(img1, 1500);
             setTimeout(img2,1750);
-            setTimeout(img3, 2000);
+            setTimeout(img1, 2000);
             setTimeout(img2,2250);
-            setTimeout(img1, 2500);
+            setTimeout(img3, 2500);
         }
     }
     else if(no === 'Paper'){
@@ -150,8 +140,19 @@ function dance(){
             setTimeout(img1, 1500);
             setTimeout(img2,1750);
             setTimeout(img3, 2000);
-            setTimeout(img2,2250);
-            setTimeout(img1, 2500);
+            setTimeout(img1,2250);
+            setTimeout(img2, 2500);
         }
     }
 }
+
+button_1.forEach((button) =>{
+    button.addEventListener('click', () =>{
+        choice = button.textContent;
+        Dance();
+        dance();
+    });
+});
+
+
+
