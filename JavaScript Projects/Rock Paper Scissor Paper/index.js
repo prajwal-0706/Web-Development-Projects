1be2d1f2bfb92fc87c636cd50efd5dcf268dddd4
prajwal-0706 <117188729+prajwal-0706@users.noreply.github.com>
2023-01-08 18:14:29 +0000

    Finished

--- JavaScript Projects/Rock Paper Scissor Paper/index.js
+++ JavaScript Projects/Rock Paper Scissor Paper/index.js
@@ -1,17 +1,30 @@
+// Declarations..
 const image = document.querySelectorAll('#image');
 const img_1 = document.querySelector('.image-1');
 const img_2 = document.querySelector('.image-2');
 const img_3 = document.querySelector('.image-3');
-const button_1 = document.querySelectorAll('button'); 
+const button = document.querySelectorAll('.button'); 
 const Img_1 = document.querySelector('.imag-1');// Paper
 const Img_2 = document.querySelector('.imag-2'); // Scissor
 const Img_3 = document.querySelector('.imag-3'); // Rock
-const button = document.querySelector('.PlayerScore');
-const but = document.querySelector('.ComputerScore');
+const Start = document.querySelector('.btn');
+const Result = document.querySelector('.result');
+const PlayerS = document.querySelector('.PlayerScore');
+const ComputerS = document.querySelector('.ComputerScore');
 let choice = '';
 const Arr = ['Rock', 'Paper', 'Scissor'];
 var no = Arr[Math.floor(Math.random() * Arr.length)];
+let PlayerScore = 0;
+let ComputerScore = 0;
 
+Start.addEventListener('click',Display);
+
+function Display(){
+    document.querySelector('.Score').style.opacity = '1';
+    document.querySelector('.select-btn').style.opacity = '1';
+    Start.style.opacity = '0';
+    Result.style.transform = 'translate(0px, 160px)';
+}
 
 function Img1(){ // Paper
     img_1.style.opacity = '1';
@@ -144,49 +157,120 @@ function dance(){
             setTimeout(img2, 2500);
         }
     }
+
 }
 
-button_1.forEach((button) =>{
+button.forEach((button) =>{
     button.addEventListener('click', () =>{
         choice = button.textContent;
         Dance();
         dance();
         setTimeout(checkwin,2550);
+        Result.style.opacity = '0';
+        setTimeout(clearImg, 2700);
     });
 });
 
+function clearImg(){
+    img_1.style.opacity = '0';
+    img_2.style.opacity = '0';
+    img_3.style.opacity = '0';
+    Img_1.style.opacity = '0';
+    Img_2.style.opacity = '0';
+    Img_3.style.opacity = '0';
+}
+
 function checkwin(){
+    let Player = false;
+    let Computer = false;
+
     if(choice === no){
         console.log("It's Tie");
+        if(Player === false && Computer === false){
+            Result.innerHTML = "It's a Tie";
+            Result.style.opacity = '1';
+            Result.style.left= '650px';
+
+        }
     }
+
     // Player Win Conditons
 
     if(choice === 'Paper' && no ==='Rock'){
-        console.log("Paper Beats Rock");
-        console.log("Player Won");
+        Player = true;
+        ++PlayerScore;
+        Computer =false;
+        if(Player === true && Computer === false){
+            Result.innerHTML = "Player Won";
+            Result.style.opacity = '1';
+            Result.style.left = '620px';
+            PlayerS.innerHTML = `Player Score - ${PlayerScore}`;
+        }
+        Player = false;
     }
     if(choice === 'Rock' && no === 'Scissor'){
-        console.log("Rock Beats Scissor");
-        console.log("Player Won");
+        Player = true;
+        Computer =false;
+        ++PlayerScore;
+        if(Player === true && Computer === false){
+            Result.innerHTML = "Player Won";
+            Result.style.opacity = '1';
+            Result.style.left = '620px';
+            PlayerS.innerHTML = `Player Score - ${PlayerScore}`;
+        }
+        Player = false;
     }
+
     if(choice === 'Scissor' && no === 'Paper'){
-        console.log("Scisssor Beats Paper");
-        console.log("Player Won");
+        Player = true;
+        Computer =false;
+        ++PlayerScore;
+        if(Player === true && Computer === false){
+            Result.innerHTML = "Player Won";
+            Result.style.opacity = '1';
+            Result.style.left = '620px';
+            PlayerS.innerHTML = `Player Score - ${PlayerScore}`;
+        }core
+        Player = false;
     }
 
     // Computer Win Condtions
 
     if(no === 'Paper' && choice ==='Rock'){
-        console.log("Paper Beats Rock");
-        console.log("Computer Won");
+        Computer = true;
+        Player = false;
+        ComputerScore++;
+        if(Computer === true && Player === false){
+            Result.innerHTML = "Computer Won";
+            Result.style.opacity = '1';
+            Result.style.left = '570px';
+            ComputerS.textContent = ` Computer Score - ${ComputerScore}`
+        }
+        Computer = false;
     }
     if(no === 'Rock' && choice === 'Scissor'){
-        console.log("Rock Beats Scissor");
-        console.log("Computer Won");
+        Computer = true;
+        Player = false;
+        ComputerScore++;
+        if(Computer === true && Player === false){
+            Result.innerHTML = "Computer Won";
+            Result.style.opacity = '1';
+            Result.style.left = '570px';
+            ComputerS.textContent = ` Computer Score - ${ComputerScore}`
+        }
+        Computer = false;
     }
     if(no === 'Scissor' && choice === 'Paper'){
-        console.log("Scisssor Beats Paper");
-        console.log(" Computer Won");
+        Computer = true;
+        Player = false;
+        ComputerScore++;
+        if(Computer === true && Player === false){
+            Result.innerHTML = "Computer Won";
+            Result.style.opacity = '1';
+            Result.style.left = '570px';
+            ComputerS.textContent = ` Computer Score - ${ComputerScore}`
+        }
+        Computer = false;
     } 
 }
 
