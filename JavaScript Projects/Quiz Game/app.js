045d4f7e200ae35f9a921d7ae74b0bfd7142af2e
prajwal-0706 <117188729+prajwal-0706@users.noreply.github.com>
2023-01-11 15:16:48 +0000

    Updated

--- JavaScript Projects/Quiz Game/app.js
+++ JavaScript Projects/Quiz Game/app.js
@@ -1,6 +1,8 @@
 const Level = document.querySelector('.by-level');
 const Topic = document.querySelector('.by-topic');
 const Start = document.querySelector('.btn');
+const levels = document.querySelectorAll('.level-1');
+
 
 Start.addEventListener('click', Initial_Beginning)
 Level.addEventListener('click',Start_by_level );
@@ -13,9 +15,14 @@ function Initial_Beginning (){
     document.querySelector('.by-topic').style.opacity = 1;
     document.querySelector('.by-topic').style.transform = "translate(-150px, 130px)";
 }
-
+function disp(Element){
+    Element.style.opacity = '1';
+}
 function Start_by_level(){
     document.querySelector('.by-level').style.opacity = 0;
     document.querySelector('.by-topic').style.opacity = 0;
+    levels.forEach((Element) => {
+       setTimeout(disp(Element), 1500);
+    });
 }
 
