be92f781d7f257736e65a2f9adcdf0c9af1772c6
prajwal-0706 <117188729+prajwal-0706@users.noreply.github.com>
2023-01-20 15:54:36 +0530

    Delete React-projects directory

--- React-projects/Hotel_Landing_page/comp.js
+++
@@ -1,4 +0,0 @@
-import react from 'react';
-import ReactDOM  from 'react-dom';
-
-ReactDOM.render( <h1> Hello </h1>,document.getElementById('root') );
--- React-projects/Hotel_Landing_page/readme.txt
+++
@@ -1,2 +0,0 @@
-
-# Here is the Clone Website of Tajhotels.com
