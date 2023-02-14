08f1db29eb16db15dcb0e586de05ee8eaf486cbe
prajwal-0706 <117188729+prajwal-0706@users.noreply.github.com>
2023-02-14 12:14:50 +0000

    Done with Cloning of Lambo image gallery

--- JavaScript Projects/Clone Website/index.js
+++ JavaScript Projects/Clone Website/index.js
@@ -3,7 +3,7 @@ const mouse = document.querySelector(".mouse-scroll");
 
 
 carousel.addEventListener("mousemove", (e) => {
-    let x = e.pageX - 48 ;
+    let x = e.pageX - 48;
     let y = e.pageY - 48;
 
     mouse.style.opacity = '1';
@@ -15,13 +15,21 @@ carousel.addEventListener("mousemove", (e) => {
 
 const select_1 = document.querySelector(".select-1");
 const select_2 = document.querySelector(".select-2");
+const config = document.querySelector(".configuration");
+const text_3 = document.querySelector(".text-3");
+
 
 select_1.addEventListener("click", () => {
     select_1.classList.add("active");
     select_2.classList.remove("active");
+    config.classList.add("active");
+    text_3.textContent = "HURACÁN";
 });
 
 select_2.addEventListener("click", () => {
     select_2.classList.add("active");
     select_1.classList.remove("active");
+    config.classList.remove("active");
+    text_3.textContent = "URUS";
+
 });
