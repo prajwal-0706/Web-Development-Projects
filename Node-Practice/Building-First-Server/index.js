1583306ce1eb644105f188388de0e6559099d88c
prajwal-0706 <prajwalgadhave356@gmail.com>
2023-03-27 18:47:57 +0530

    Update

--- Node-Practice/Building-First-Server/index.js
+++ Node-Practice/Building-First-Server/index.js
@@ -16,6 +16,19 @@ app.get('/calculator', (req, res) => {
   res.sendFile(__dirname + '/calculator.html');
 });
 
+app.get('/bmi', (req, res) => {
+  res.sendFile(__dirname + '/bmi.html');
+});
+
+app.post('/bmi', (req, res) => {
+  let height = parseFloat(req.body.height);
+  let weight = parseFloat(req.body.weight);
+
+  let bmi = height / (weight * weight);
+
+  res.send(`Answer is ${bmi}`);
+});
+
 app.post('/calculator', (req, res) => {
   let n1 = parseInt(req.body.num1);
   let n2 = parseInt(req.body.num2);
